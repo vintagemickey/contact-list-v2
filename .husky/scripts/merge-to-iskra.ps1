9a056bf12f83a1e79bf98d0def2fe305e534d764
Evgeniya Mayorova <evgeniya.mayorova@ladadigit.io>
2025-10-01 23:13:22 +0500

    fix

--- .husky/scripts/merge-to-iskra.ps1
+++ .husky/scripts/merge-to-iskra.ps1
@@ -28,42 +28,44 @@ $timestamp = [int][double]::Parse((Get-Date -UFormat %s))
 $tmpBranch = "tmp/merge-$($CurrentBranch.Replace('/', '-'))-$timestamp"
 
 Write-Host ">>> Trying to checkout temporary branch $tmpBranch from $Remote/$TestBranch ..."
-try {
+try
+{
     git checkout -b $tmpBranch "$Remote/$TestBranch"
-    Write-Host ">>> Successfully created temporary branch $tmpBranch"
+    # Write-Host ">>> Successfully created temporary branch $tmpBranch"
 }
-catch {
-    Write-Host "Remote $Remote/$TestBranch not found — creating from $Remote/$MainBranch"
+catch
+{
+    # Write-Host "Remote $Remote/$TestBranch not found — creating from $Remote/$MainBranch"
     git checkout -b $tmpBranch "$Remote/$MainBranch"
-    Write-Host ">>> Successfully created temporary branch $tmpBranch from $MainBranch"
+    # Write-Host ">>> Successfully created temporary branch $tmpBranch from $MainBranch"
 }
 
-# Merge master
-Write-Host ">>> Merging $Remote/$MainBranch..."
-if (-not (git merge --no-edit "$Remote/$MainBranch")) {
-    Write-Host "Conflict with $MainBranch — aborting."
-    git merge --abort | Out-Null
-    git checkout $CurrentBranch | Out-Null
-    git branch -D $tmpBranch | Out-Null
-    exit 1
-}
+# # Merge master
+# Write-Host ">>> Merging $Remote/$MainBranch..."
+# if (-not (git merge --no-edit "$Remote/$MainBranch")) {
+#     Write-Host "Conflict with $MainBranch — aborting."
+#     git merge --abort | Out-Null
+#     git checkout $CurrentBranch | Out-Null
+#     git branch -D $tmpBranch | Out-Null
+#     exit 1
+# }
 
-# Merge текущей ветки
-Write-Host ">>> Merging $CurrentBranch..."
-if (-not (git merge --no-edit $CurrentBranch)) {
-    Write-Host "Conflict with $CurrentBranch — aborting."
-    git merge --abort | Out-Null
-    git checkout $CurrentBranch | Out-Null
-    git branch -D $tmpBranch | Out-Null
-    exit 1
-}
+# # Merge текущей ветки
+# Write-Host ">>> Merging $CurrentBranch..."
+# if (-not (git merge --no-edit $CurrentBranch)) {
+#     Write-Host "Conflict with $CurrentBranch — aborting."
+#     git merge --abort | Out-Null
+#     git checkout $CurrentBranch | Out-Null
+#     git branch -D $tmpBranch | Out-Null
+#     exit 1
+# }
 
-# Пушим в iskra
-Write-Host ">>> Pushing to $Remote/$TestBranch..."
-git push $Remote "HEAD:$TestBranch"
+# # Пушим в iskra
+# Write-Host ">>> Pushing to $Remote/$TestBranch..."
+# git push $Remote "HEAD:$TestBranch"
 
-# Возвращаемся обратно и удаляем временную
-git checkout $CurrentBranch | Out-Null
-git branch -D $tmpBranch | Out-Null
+# # Возвращаемся обратно и удаляем временную
+# git checkout $CurrentBranch | Out-Null
+# git branch -D $tmpBranch | Out-Null
 
-Write-Host ">>> Done! Merged into $Remote/$TestBranch"
+# Write-Host ">>> Done! Merged into $Remote/$TestBranch"
